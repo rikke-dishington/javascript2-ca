191eef68f25c20d2694abddd6f2beab85e6ca402
rikke-dishington <85433495+rikke-dishington@users.noreply.github.com>
2023-09-17 20:18:57 +0200

    Add JSDoc

--- js/signin.mjs
+++ js/signin.mjs
@@ -1,7 +1,26 @@
+/**
+ * The base URL for API requests.
+ * @type {string}
+ */
 const API_BASE_URL = "https://api.noroff.dev";
 
+/**
+ * The HTML element used to display error messages.
+ * @type {HTMLElement}
+ */
 const errorMessageElement = document.getElementById("error-message");
 
+/**
+ * Attempts to log in a user by sending a POST request to the specified URL with user data.
+ *
+ * @async
+ * @function loginUser
+ * @param {string} url - The URL to which the login request is sent.
+ * @param {Object} data - An object containing user login data.
+ * @param {string} data.email - The user's email.
+ * @param {string} data.password - The user's password.
+ * @throws {Error} If an error occurs during the login process or network request.
+ */
 async function loginUser(url, data) {
   try {
     const postData = {
@@ -35,6 +54,11 @@ async function loginUser(url, data) {
   }
 }
 
+/**
+ * Event listener for the "Sign In" button click event.
+ * Retrieves user input, calls loginUser, and initiates login.
+ * @event
+ */
 document.getElementById("signin-button").addEventListener("click", () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
